d366d05dcf2437ccf38bdcd6e7de47511255d19b
Flowency <jason@flowency.co.uk>
2026-08-18 20:56:20 +0100

    Add festival discovery to gigs page

--- src/app/gigs/page.tsx
+++ src/app/gigs/page.tsx
@@ -1,8 +1,10 @@
 import { GigsHome } from "@/features/gigs/GigsHome";
+import { GigsFestivalStrip } from "@/features/festivals/GigsFestivalStrip";
 
 export default function GigsPage() {
   return (
     <div className="bndy-gigs-page">
+      <GigsFestivalStrip />
       <GigsHome />
     </div>
   );
