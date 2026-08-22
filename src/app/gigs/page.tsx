969899409f45bf11d371b0be958b4f933617a0d4
Flowency <jason@flowency.co.uk>
2026-08-22 15:35:21 +0100

    Redirect legacy gigs route on brass build

--- src/app/gigs/page.tsx
+++ src/app/gigs/page.tsx
@@ -1,7 +1,9 @@
+import { redirect } from "next/navigation";
 import { GigsHome } from "@/features/gigs/GigsHome";
 import { GigsFestivalStrip } from "@/features/festivals/GigsFestivalStrip";
 
 export default function GigsPage() {
+  if (process.env.NEXT_PUBLIC_BNDY_EDITION === "brass") redirect("/concerts");
   return (
     <div className="bndy-gigs-page">
       <GigsFestivalStrip />
