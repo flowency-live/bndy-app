261a546e252cd0997fb2e0e191087e134ac4a16d
Flowency <jason@flowency.co.uk>
2026-08-29 13:35:01 +0100

    Remove Claim V2 post-apply helper

--- scripts/post-claim-v2-fix.js
+++
@@ -1,5 +0,0 @@
-const fs=require('fs');
-let p='src/features/join/ClaimEvidenceStep.tsx';let s=fs.readFileSync(p,'utf8');
-s=s.replace('trackJoin("claim_requested",{entityType,step:"claim",verification:"manual",role:requestedRole});','trackJoin("claim_requested",{entityType,step:"claim",result:"manual_evidence"});');
-fs.writeFileSync(p,s);
-p='src/features/join/JoinVenueFlow.tsx';s=fs.readFileSync(p,'utf8');s=s.replaceAll(' setClaimSubmitted(false);','').replaceAll('setClaimSubmitted(false);','');fs.writeFileSync(p,s);
